986c510b9f8f52c707b0237f5e4d655c6223d092
Jeffrey <cheng.chilap.221@gmail.com>
2021-03-22 15:28:56 +0800

    reverse testing

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          📸  攝影吹你老母
+          📸  攝影吹
         </h1>
         <p className="description">
         The arts, the tech, the gears, the business.... ALL about photography.<br/>關於攝影，無所不談；無所不吹。 <br/> 不論你係專業攝影師，定係攝影愛好者，入嚟吹吓攝影！
