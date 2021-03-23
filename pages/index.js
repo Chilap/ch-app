1e09f63acb6e17261880ed7fe867781b9c61a1aa
Chilap <cheng.chilap.221@gmail.com>
2021-03-24 01:09:55 +0800

    google slides added

--- pages/index.js
+++ pages/index.js
@@ -178,6 +178,7 @@ export default function Home() {
           </div>
         
           </div>
+          <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR8neJVMHjwZwyDbMGGAd_u5j6IY9VkRRbVgfA8FxQ09b1plvjMDJLHA6EFaSS09MVl234idSS4QTkw/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
        </main>
 
       <div className='footer'>
