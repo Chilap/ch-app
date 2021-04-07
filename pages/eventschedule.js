6e322c1b260659ea4ceed15fd46355e2662aca65
Chilap <cheng.chilap.221@gmail.com>
2021-04-08 03:33:48 +0800

    haha schedule

--- pages/eventschedule.js
+++ pages/eventschedule.js
@@ -2,13 +2,101 @@ import Nav from "./nav";
 
 export default function EventSchedule() {
   return (
-    <div id="content">
-      <h1 className="text-4xl">0424 交配gogogo</h1>
-      <Nav />
-      <div className = 'inline-block lg:hidden'>
+    <div>
+      <div className="inline-block lg:hidden w-screen text-center">
+        <h1 className="text-4xl m-6">0424 Event</h1>
+        <Nav />
         <p>In Progress... Use web to see the schedule</p>
+
+        <div className="grid grid-cols-3 p-6 m-6 text-left">
+          <div className="left">
+            <div className="time">
+              <p>07:00 - 07:45</p>
+            </div>
+            <div className="speaker">
+              <p>Yankov Wong</p>
+            </div>
+          </div>
+
+          <div className="right col-span-2">
+            <div className="text-left">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mi
+                nibh, efficitur at ante sit amet, hendrerit gravida metus. Fusce
+                ipsum diam, auctor vitae vestibulum id, interdum vitae velit. In
+                auctor dictum magna, vitae pretium ex commodo id.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 p-6 m-6 text-left">
+          <div className="left">
+            <div className="time">
+              <p>07:00 - 07:45</p>
+            </div>
+            <div className="speaker">
+              <p>Yankov Wong</p>
+            </div>
+          </div>
+
+          <div className="right col-span-2">
+            <div className="text-left">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mi
+                nibh, efficitur at ante sit amet, hendrerit gravida metus. Fusce
+                ipsum diam, auctor vitae vestibulum id, interdum vitae velit. In
+                auctor dictum magna, vitae pretium ex commodo id.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 p-6 m-6 text-left">
+          <div className="left">
+            <div className="time">
+              <p>07:00 - 07:45</p>
+            </div>
+            <div className="speaker">
+              <p>Yankov Wong</p>
+            </div>
+          </div>
+
+          <div className="right col-span-2">
+            <div className="text-left">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mi
+                nibh, efficitur at ante sit amet, hendrerit gravida metus. Fusce
+                ipsum diam, auctor vitae vestibulum id, interdum vitae velit. In
+                auctor dictum magna, vitae pretium ex commodo id.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 p-6 m-6 text-left">
+          <div className="left">
+            <div className="time">
+              <p>07:00 - 07:45</p>
+            </div>
+            <div className="speaker">
+              <p>Yankov Wong</p>
+            </div>
+          </div>
+
+          <div className="right col-span-2">
+            <div className="text-left">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mi
+                nibh, efficitur at ante sit amet, hendrerit gravida metus. Fusce
+                ipsum diam, auctor vitae vestibulum id, interdum vitae velit. In
+                auctor dictum magna, vitae pretium ex commodo id.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="hidden lg:inline-block">
+
+      <div id="content" className="hidden lg:inline-block">
+        <h1 className="text-4xl">0424 交配gogogo</h1>
+        <Nav />
         <ul className="timeline">
           <li className="event" data-date="7:00am - 07:45am">
             <h3>Lighting</h3>
