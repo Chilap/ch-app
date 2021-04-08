fa0a14af2cecb2363dbff6b91949f18b53177eb6
Chilap <cheng.chilap.221@gmail.com>
2021-04-08 13:06:43 +0800

    season

--- pages/eventschedule.js
+++ pages/eventschedule.js
@@ -7,28 +7,42 @@ export default function EventSchedule() {
   return (
     <div>
       {/* -------------------------- Phone -------------------------- */}
-      <div className="w-screen text-center p-6 md:px-40 lg:px-60">
-        <h1 className="text-4xl m-6">0424 交配gogogo</h1>
+      <div className="w-screen p-6 md:px-40 lg:px-60">
+        <h1 className="text-4xl m-6 text-center">0424 交配gogogo</h1>
+        <div className = 'text-center'>
         <Nav />
+        </div>
+        
         <div className="mt-12">
+          <p className = 'my-12'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio erat, consequat in est non, vestibulum dictum lacus. 
+            Fusce commodo sollicitudin rhoncus. Pellentesque a quam in turpis tincidunt consequat. Sed a viverra arcu, a pulvinar quam. Etiam lobortis elit eu sem mollis finibus eu nec ligula. 
+            Nunc sollicitudin varius velit, sed lacinia est pellentesque in. Praesent at sollicitudin lectus. Curabitur lacinia euismod neque eu pellentesque. Donec ac imperdiet purus. Maecenas vel malesuada purus. 
+            Duis laoreet mi vehicula, tempus urna nec, pulvinar ligula. Nulla id vulputate leo. Suspendisse auctor velit id sapien sollicitudin ultrices. 
+            <br/>
+            <br/>
+            👋 See you online!
+          </p>
           {/* ------------- Section ------------- */}
-          <div className="grid  p-6 m-6 text-left bg-white rounded-2xl shadow duration-150 hover:shadow-lg">
+          <h1 className = 'text-center text-3xl'>🗓 Timetable</h1>
+          <div className="grid p-6 mt-3 my-6 text-left bg-white rounded-2xl shadow-md duration-150 ">
             {/* ------------- Top ------------- */}
             <div>
-              <p className="text-gray-400 mb-3">⏱ THU, 21 March, 07:00 (HKT)</p>
+              <p className="text-gray-400 mb-1.5">⏱ THU, 21 March, 07:00 (HKT)</p>
             </div>
 
             <div className="grid grid-cols-3">
               {/* ------------- Left ------------- */}
               <div className="">
                 <div className="speaker">
-                  <p>✳️ Yankov Wong</p>
+                  <p>✳️ Yankov <span className = 'hidden md:inline'>Wong</span></p>
                   <Image
                     className="topSpeakerIcon"
                     src="/images/Yankov_icon.png"
                     width={100}
                     height={100}
                   />
+                  
                 </div>
               </div>
               {/* ------------- Right ------------- */}
@@ -36,20 +50,17 @@ export default function EventSchedule() {
                 <div className="text-left">
                   <h1 className="text-2xl mb-3">💡 Lighting</h1>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    mi nibh, efficitur at ante sit amet, hendrerit gravida
-                    metus. Fusce ipsum diam, auctor vitae vestibulum id,
-                    interdum vitae velit. In auctor dictum magna, vitae pretium
-                    ex commodo id.
+                  🎯 Inverse-square law <br/>
+                  🎯 hello
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="grid  p-6 m-6 text-left bg-white rounded-2xl shadow duration-150 hover:shadow-lg">
+          <div className="grid p-6 mt-3 my-6 text-left bg-white rounded-2xl shadow-md duration-150 ">
             {/* ------------- Top ------------- */}
             <div>
-              <p className="text-gray-400 mb-3">⏱ THU, 21 March, 07:00 (HKT)</p>
+              <p className="text-gray-400  mb-1.5">⏱ THU, 21 March, 07:00 (HKT)</p>
             </div>
 
             <div className="grid grid-cols-3">
@@ -58,7 +69,7 @@ export default function EventSchedule() {
                 <div className="speaker">
                   <p>✳️ Charlie Kayi</p>
                   <Image
-                    className="topSpeakerIcon"
+                    className="topSpeakerIcon "
                     src="/images/Charlie_icon.png"
                     width={100}
                     height={100}
@@ -80,20 +91,20 @@ export default function EventSchedule() {
               </div>
             </div>
           </div>
-          <div className="grid  p-6 m-6 text-left bg-white rounded-2xl shadow duration-150 hover:shadow-lg">
+          <div className="grid p-6 mt-3 my-6 text-left bg-white rounded-2xl shadow-md duration-150 ">
             {/* ------------- Top ------------- */}
             <div>
-              <p className="text-gray-400 mb-3">⏱ THU, 21 March, 07:00 (HKT)</p>
+              <p className="text-gray-400  mb-1.5">⏱ THU, 21 March, 07:00 (HKT)</p>
             </div>
 
             <div className="grid grid-cols-3">
               {/* ------------- Left ------------- */}
               <div className="">
                 <div className="speaker">
-                  <p>✳️ Seaons Wong</p>
+                  <p>✳️ Season <span className = 'hidden md:inline'>Wong</span></p>
                   <Image
                     className="topSpeakerIcon"
-                    src="/images/Yankov_icon.png"
+                    src="/images/Season_icon.png"
                     width={100}
                     height={100}
                   />
@@ -114,10 +125,10 @@ export default function EventSchedule() {
               </div>
             </div>
           </div>
-          <div className="grid  p-6 m-6 text-left bg-white rounded-2xl shadow duration-150 hover:shadow-lg">
+          <div className="grid p-6 mt-3 my-6 text-left bg-white rounded-2xl shadow-md duration-150 ">
             {/* ------------- Top ------------- */}
             <div>
-              <p className="text-gray-400 mb-3">⏱ THU, 21 March, 07:00 (HKT)</p>
+              <p className="text-gray-400 mb-1.5">⏱ THU, 21 March, 07:00 (HKT)</p>
             </div>
 
             <div className="grid grid-cols-3">
