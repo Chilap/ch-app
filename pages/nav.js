aaac781850c4db85a293205f2627ea1352103099
Chilap <cheng.chilap.221@gmail.com>
2021-04-09 14:54:46 +0800

    nav bar updated

--- pages/nav.js
+++ pages/nav.js
@@ -2,20 +2,24 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <ul className="m-8 text-center">
+    <ul className="m-8 text-center border-2 rounded-full p-3 border-gray-800">
       <li className='inline-block px-3 underline'>
         <Link href="/">
-          <a>Home</a>
+          <a>🏠 Home</a>
         </Link>
       </li>
+
+      <br className='inline-block md:hidden'/>
 			<li className='inline-block  px-3 underline'>
         <Link href="/eventschedule">
-          <a>24April Event</a>
+          <a>🎫 24April Event</a>
         </Link>
       </li>
+
+      <br className='inline-block md:hidden'/>
       <li className='inline-block px-3 underline'>
         <Link href="/notes">
-          <a>Clubhouse Notes</a>
+          <a>📝 Clubhouse Notes</a>
         </Link>
       </li>
     </ul>
