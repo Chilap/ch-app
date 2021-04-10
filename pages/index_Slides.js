bc09e99f4d073a8e7c81d1b9d04fb6dee0d98088
Chilap <cheng.chilap.221@gmail.com>
2021-04-10 15:26:03 +0800

    slides responsive

--- pages/index_Slides.js
+++ pages/index_Slides.js
@@ -1,11 +1,31 @@
 export default function Presentation() {
     return(
-        <div className="presentation hidden lg:inline-block">
+        <div className="presentation">
             <h3><em>"Today's Title"</em> Slides</h3>
             <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR8neJVMHjwZwyDbMGGAd_u5j6IY9VkRRbVgfA8FxQ09b1plvjMDJLHA6EFaSS09MVl234idSS4QTkw/embed?start=false&loop=false&delayms=3000" 
-                className = "presentationSlides"
+                className = "presentationSlides hidden lg:inline-block"
                 frameborder="0" 
                 width="960" height="569" 
+                allowfullscreen="true" 
+                mozallowfullscreen="true" 
+                webkitallowfullscreen="true"
+            />
+            
+            {/* tablet */}
+            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR8neJVMHjwZwyDbMGGAd_u5j6IY9VkRRbVgfA8FxQ09b1plvjMDJLHA6EFaSS09MVl234idSS4QTkw/embed?start=false&loop=false&delayms=3000" 
+                className = "presentationSlides hidden md:inline-block lg:hidden"
+                frameborder="0" 
+                width="700" height="440" 
+                allowfullscreen="true" 
+                mozallowfullscreen="true" 
+                webkitallowfullscreen="true"
+            />
+
+            {/* phone */}
+            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR8neJVMHjwZwyDbMGGAd_u5j6IY9VkRRbVgfA8FxQ09b1plvjMDJLHA6EFaSS09MVl234idSS4QTkw/embed?start=false&loop=false&delayms=3000" 
+                className = "presentationSlides inline-block md:hidden"
+                frameborder="0" 
+                width="380" height="250" 
                 allowfullscreen="true" 
                 mozallowfullscreen="true" 
                 webkitallowfullscreen="true"
