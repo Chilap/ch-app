a3a617aee997283b1b5a8b0b2084ca5d4558ae8d
Chilap <cheng.chilap.221@gmail.com>
2021-04-20 11:15:05 +0800

    maybe next time

--- pages/popup.js
+++ pages/popup.js
@@ -113,7 +113,7 @@ export default function Popup() {
                   className="mx-2 inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
                   onClick={closeModal}
                 >
-                  😫 Nah..
+                  🙂 Maybe next time
                 </button>
               </div>
             </div>
